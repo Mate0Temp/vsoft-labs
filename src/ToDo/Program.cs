5361612781703d9427e3a0937679a39b1dd9ce6f
Mate0Temp <mateusz.podoba@vsoft.pl>
2024-11-21 11:01:19 +0000

    PULLLL

--- src/ToDo/Program.cs
+++ src/ToDo/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApiDocument(config =>
 {
     config.DocumentName = "TodoAPI";
-    config.Title = "TodoAPI v1";
+    config.Title = "TodoAPI v1 PULLLLLLLL";
     config.Version = "v1";
 });
 
