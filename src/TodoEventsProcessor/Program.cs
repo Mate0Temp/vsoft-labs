37241fb78869f81de183be7fecfc7c256d310ee7
Mate0Temp <mateusz.podoba@vsoft.pl>
2024-11-21 14:20:29 +0000

    next...

--- src/TodoEventsProcessor/Program.cs
+++ src/TodoEventsProcessor/Program.cs
@@ -2,6 +2,11 @@ using Azure.Identity;
 using Azure.Messaging.ServiceBus;
 using Azure.Security.KeyVault.Secrets;
 using Azure.Storage.Blobs;
+using Microsoft.ApplicationInsights.AspNetCore.Extensions;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.Extensibility.PerfCounterCollector;
+using Microsoft.ApplicationInsights.DependencyCollector;
+using Microsoft.ApplicationInsights.Kubernetes;
 
 var builder = Host.CreateApplicationBuilder(args);
 
@@ -26,6 +31,18 @@ var serviceBusClient = new ServiceBusClient(serviceBusConnection);
 builder.Services.AddSingleton(serviceBusClient);
 
 builder.Services.AddHostedService<Worker>();
+
+builder.Services.AddApplicationInsightsTelemetry(options =>
+{
+    options.EnableAdaptiveSampling = false; // Wyłączenie adaptacyjnego samplingu
+    options.EnableDependencyTrackingTelemetryModule = true; // Distributed tracing
+    options.EnablePerformanceCounterCollectionModule = true; // Metryki wydajności
+    options.EnableAppServicesHeartbeatTelemetryModule = true; // Heartbeat
+    options.EnableDebugLogger = true; // Debugowanie w konsoli (dla deweloperów)
+});
+
+// Dodanie Kubernetes Enricher
+builder.Services.AddApplicationInsightsKubernetesEnricher();
 
 var host = builder.Build();
 host.Run();
